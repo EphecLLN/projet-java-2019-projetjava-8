a5dadcd1236661f0e04bc16c5efb5e51004d4fc3
CornilJean2TL2 <33150074+CornilJean2TL2@users.noreply.github.com>
2019-11-15 17:55:09 +0100

    tesEchange

--- src/projet/JoueursTest.java
+++ src/projet/JoueursTest.java
@@ -45,15 +45,15 @@ class JoueursTest {
 		j.estVisible(c1,0);
 		int[][] carteJ = j.getCartesVisibles();
 		/**
-		 * test le nombre de cartes
+		 * test: le nombre de cartes
 		 */
 		assertEquals(7,j.getNbrCartes());
 		/**
-		 * test le int correspondant a la couleur
+		 * test: le int correspondant a la couleur
 		 */
 		assertEquals(2,carteJ[0][0]);
 		/**
-		 * test le int correspondant a la valeur
+		 * test: le int correspondant a la valeur
 		 */
 		assertEquals(3,carteJ[0][1]);
 	}
@@ -64,16 +64,38 @@ class JoueursTest {
 		j.estEnMain(c1,0);
 		int[][] carteJ = j.getCartesMain();
 		/**
-		 * test le nombre de cartes
+		 * test: le nombre de cartes
 		 */
 		assertEquals(5,j.getNbrCartes());
 		/**
-		 * test le int correspondant a la couleur
+		 * test: le int correspondant a la couleur
 		 */
 		assertEquals(1,carteJ[0][0]);
 		/**
-		 * test le int correspondant a la valeur
+		 * test: le int correspondant a la valeur
 		 */
 		assertEquals(12,carteJ[0][1]);
 	}
+	@Test
+	void testEchange() {
+		Carte c1 = new Carte(1,12);
+		Joueurs j = new Joueurs(1,5);
+		String etat = "main";
+		int[][] carteJ = {{0,0},{0,0},{0,0}};
+		j.echange(c1, 1, etat, 0);
+		/**
+		 * test : echange d'une carte selon son etat
+		 */
+		switch (etat) {
+		case "visible":
+			carteJ= j.getCartesVisibles();
+		case "cachees":
+			carteJ = j.getCartesCachees();
+		case "main":
+			carteJ = j.getCartesMain();
+		}
+		assertEquals(1,carteJ[0][0]);
+		assertEquals(12,carteJ[0][1]);
+	}
+	
 }
