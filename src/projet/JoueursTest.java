6319dd89ef5b04d93ded68fd37d317502d8658c5
CornilJean2TL2 <33150074+CornilJean2TL2@users.noreply.github.com>
2019-11-15 15:39:16 +0100

    creationtest unitaires classe joueur

---
+++ src/projet/JoueursTest.java
@@ -0,0 +1,14 @@
+package projet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JoueursTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
